221d22ccf9b050ff3604a94997a772ee8fac0d82
Juan Stiven Muñoz Garcia <juan.98mg@gmail.com>
2021-02-28 16:10:05 -0500

    create app constant

---
+++ src/util/constants.js
@@ -0,0 +1,9 @@
+const appConstants = {
+  DEFAULT_HEIGHT: "15px",
+  DEFAULT_WIDTH: "15px",
+  COLLECTION_PETS: "pets",
+  SUCCES_SAVED_MESSAGE: "Mascota Guardada con exito",
+  SUCCES_UPDATD_MESSAGE: "Mascota actualizada con exito",
+};
+
+export default appConstants;
